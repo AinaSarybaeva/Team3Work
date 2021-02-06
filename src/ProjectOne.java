b7e6c769f2b769feb430c2a9632fe9f8aaa3b42d
FlorenciaEspinosa <77857368+FlorenciaEspinosa@users.noreply.github.com>
2021-02-06 08:58:35 -0500

    Update ProjectOne.java
    last part 15- 16- 17

--- src/ProjectOne.java
+++ src/ProjectOne.java
@@ -147,6 +147,38 @@ public class ProjectOne {
         driver.findElement(By.id("menu_admin_viewAdminModule")).click();
         driver.findElement(By.id("menu_news_Announcements")).click();
         driver.findElement(By.id("menu_news_viewNewsList")).click();
+            
+        // 15. Check the item and delete it
+            
+        String text = driver.findElement(By.xpath("//*[@id=\"resultTable\"]/tbody/tr[1]/td[2]/a")).getText();
+        driver.findElement(By.xpath("(//tr[@class='dataRaw']//td//label)[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"frmList_ohrmListComponent_Menu\"]/i")).click();
+        driver.findElement(By.id("newsDelete")).click();
+        driver.findElement(By.id("news-delete-button")).click();
+        
+         // 16. Verify that item doesn"t exist in the table anymore
+        
+            if (driver.findElement(By.xpath("//*[@id=\"resultTable\"]/tbody/tr[1]/td[2]/a")).equals(text)) {
+          System.out.println(text + " name still exists");
+            } else {
+         System.out.println(text + " name does not exist");
+        }
+            
+        
+       // 17. Verify row count is one less after delete
+            
+        String rowCountBefore = driver.findElement(By.id("frmList_ohrmListComponenttotal")).getText();
+    int rowCountBeforeInt = Integer.parseInt(rowCountBefore);
+System.out.println(rowCountBeforeInt);
+Thread.sleep(5000);
+String rowCountAfter = driver.findElement(By.id("frmList_ohrmListComponenttotal")).getText();
+int rowCountAfterInt = Integer.parseInt(rowCountAfter);
+System.out.println(rowCountAfterInt);
+if ((rowCountBeforeInt - 1) == rowCountAfterInt) {
+    System.out.println("Row count is one less!");
+} else {
+    System.out.println("Row count is the same!");
+}    
 
         Thread.sleep(3000);
         driver.close();
